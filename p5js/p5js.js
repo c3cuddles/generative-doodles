7c1895a092e8a98d1f63eed5392b6f9bea82dffb
Paige Weber <paigeweber31@gmail.com>
2025-10-18 02:25:19 -0500

    that's a start! generates squares of random sizes :)

--- p5js/p5js.js
+++ p5js/p5js.js
@@ -1,6 +1,12 @@
 function setup() {
-  createCanvas(180, 180);
+  HEIGHT = 180;
+  WIDTH = 180;
+
+  createCanvas(WIDTH, HEIGHT);
   background(220);
+
+  /*
+  // playing around...
   // line(30, 30, 65, random(0, 100));
   
   // lines seem to be of width 1 
@@ -11,9 +17,10 @@ function setup() {
   line (0,4, 30, 4)
   
   line (32, 4, 32, 4)
-  
+  */
+
   // 36 sections of 30,30 each. So 6 sections across, 6 down
-  
+
   /* procedure: 
   min = 5
   
@@ -43,8 +50,74 @@ function setup() {
   - doodle is random one of those colors
   - if doodle has multiple elements, make main shape (i.e. trianbgle) one color and the other lines another color
   */
-  
+
+  // remainingHeight acts as a sort of scanline for where we are in the drawing
+  remainingHeight = HEIGHT;
+  MIN = 5;
+  MAX_BLOCK_SIZE = 30;
+
+  while (remainingHeight > MIN) {
+    // no need to edit a constant! Just do Math.min instead
+    // if (remainingHeight < MAX_BLOCK_SIZE) MAX_BLOCK_SIZE = remainingHeight;
+    thisBlockHeight = random(MIN, Math.min(remainingHeight, MAX_BLOCK_SIZE));
+
+    // similarly, remainingWidth is our vertical scan line
+    remainingWidth = WIDTH;
+    while (remainingWidth > 0) {
+      thisBlockWidth = random(MIN, Math.min(remainingWidth, MAX_BLOCK_SIZE));
+
+      // randomly pick a doodle and make it!
+      generate_doodle(
+        WIDTH - remainingWidth,
+        HEIGHT - remainingHeight,
+        thisBlockWidth,
+        thisBlockHeight
+      );
+
+      remainingWidth -= thisBlockWidth;
+      remainingWidth -= 2; // give some space!
+    }
+
+    remainingHeight -= thisBlockHeight; // move scan line past this block
+    remainingHeight -= 2; // skip one line for pretty spacing :)
+  }
+
+  while (remainingHeight > 0) {
+    // do something to fill remaining?
+    line(0, remainingHeight, WIDTH, remainingHeight);
+    remainingHeight -= 2;
+  }
 }
 
-function draw() {
+/**
+ * Makes a doodle!
+ *
+ * In the case of square, will choose the min(width, height) for size.
+ *
+ * @param {*} x
+ * @param {*} y
+ * @param {*} width
+ * @param {*} height
+ */
+function generate_doodle(x, y, width, height) {
+  console.log("generating doodle...");
+  NUM_POSSIBLE_DOODLES = 1;
+  chosenDoodle = random(1, NUM_POSSIBLE_DOODLES);
+
+  switch (chosenDoodle) {
+    case 1: // square
+      console.log("chose square!");
+      square(x, y, Math.min(width, height));
+      break;
+
+    default:
+      // this should never happen! Whoops
+      console.log(
+        "Invalid doodle selected in generate_doodle, switch(chosenDoodle)"
+      );
+      break;
+  }
+  console.log("done generating doodle :)");
 }
+
+function draw() {}
